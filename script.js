f06a742c4d5ca1f36e429dec5756f3927f8170d6
Elementary OS 2022 <realgarcilaso@outlook.com>
2022-02-25 17:34:01 -0500

    Copy settings from Win81

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 //UNSPLASH API
-const count= 10;
-const apiKey='2tUkfu5pgzzYshdQi_rqHhmXuQPb2pXLMfnTir361yY';
-const apiUrl =`https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+const count = 10;
+const apiKey = "2tUkfu5pgzzYshdQi_rqHhmXuQPb2pXLMfnTir361yY";
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
 //get photos from unsplash API
 async function getPhotos() {
-    try {      const response = await fetch(apiUrl);
-      const data = await response.json();
-      console.log(data);
-    } catch (error) {
-    
-    }
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {}
 }
+
 // on load
 getPhotos();
