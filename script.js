53740ef48491b6ad317e258bb9c3d5af354c251c
Manu Elementary 2022 <realgarcilaso@outlook.com>
2022-02-26 08:17:03 -0500

    falta corregir

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
+const imageContainer = document.getElementById("image-container");
+const loader = document.getElementById("loader");
+
+let photosArray = [];
 //UNSPLASH API
 const count = 10;
-const apiKey = "2tUkfu5pgzzYshdQi_rqHhmXuQPb2pXLMfnTir361yY";
+const apiKey = '2tUkfu5pgzzYshdQi_rqHhmXuQPb2pXLMfnTir361yY';
+
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
 //get photos from unsplash API
 async function getPhotos() {
   try {
     const response = await fetch(apiUrl);
-    const data = await response.json();
-    console.log(data);
-  } catch (error) {}
+    photosArray = await response.json();
+    console.log(photosArray);
+    /* displayPhotos(); */
+  } catch (error) {
+    //cath error here
+  }
 }
 
 // on load
